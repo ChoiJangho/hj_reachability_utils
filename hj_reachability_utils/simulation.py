6f0664b0c710e1b91997cfd84715bddb496c92ab
jason <jhchoi0709@gmail.com>
2024-04-04 17:07:15 -0700

    extract extra info of controller.

--- hj_reachability_utils/simulation.py
+++ hj_reachability_utils/simulation.py
@@ -41,6 +41,7 @@ def rollout_controller(x0, dynamics, controller, t_simulation, dt=0.005, exit_ev
     us = []
     ts = []
     dxs = []
+    extras_combined = {}
     x = x0
     t = 0
     xs.append(x)
@@ -49,6 +50,11 @@ def rollout_controller(x0, dynamics, controller, t_simulation, dt=0.005, exit_ev
         # Simulate dynamics for one timestep.
         u, extras = controller(t, x)
         us.append(u)
+        for key, value in extras.items():
+            if key in extras_combined:
+                extras_combined[key].append(value)
+            else:
+                extras_combined[key] = [value]
         x, t, on_exit_cond, dx = simulate(dynamics, t, x, u, dt, exit_event_func=exit_event_func)
         # TODO: later
         # If on the switching surface, if so, reset.
@@ -65,4 +71,9 @@ def rollout_controller(x0, dynamics, controller, t_simulation, dt=0.005, exit_ev
     # add one more control to match the size of xs, us, ts.
     u_final_state, extras = controller(t, x)
     us.append(u_final_state)
-    return np.asarray(ts), np.asarray(xs).T, np.asarray(us).T, np.asarray(dxs).T
+    for key, value in extras.items():
+        if key in extras_combined:
+            extras_combined[key].append(value)
+        else:
+            extras_combined[key] = [value]
+    return np.asarray(ts), np.asarray(xs).T, np.asarray(us).T, np.asarray(dxs).T, extras_combined
